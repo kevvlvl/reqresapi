11af7465f4f4599a538ab0f9e57095b202958ce7
Kevork <ksayad@gmail.com>
2021-01-16 00:35:23 -0500

    added filter got response

---
+++ src/main/java/com/kevvlvl/rest/reqresapi/filter/ResponseFilter.java
@@ -0,0 +1,25 @@
+package com.kevvlvl.rest.reqresapi.filter;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/api/msg")
+@Slf4j
+public class ResponseFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+
+        log.info("doFilter - update Response header");
+
+        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+        httpServletResponse.setHeader("APP-REQUEST-STATUS", "Success");
+        filterChain.doFilter(servletRequest, servletResponse);gi
+    }
+}
